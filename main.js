b77eb3540fdf4e980a61a9f059b2db5d209b1eaf
karen <k.valeria.gonzalez@gmail.com>
2021-03-23 11:17:56 -0400

    adds aslide in animation

--- main.js
+++ main.js
@@ -6,3 +6,30 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+const sliders = document.querySelectorAll(".slideIn");
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "110px 110px -250px 110px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
